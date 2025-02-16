9d18cc28d6a1da9eb10d92fbb685d55559af9e17
hatmix <67128187+hatmix@users.noreply.github.com>
2025-02-16 16:53:32 -0500

    Added snow shader FPS limiter for visual aesthetic

--- main.gd
+++ main.gd
@@ -1,1 +1,21 @@
 extends Node
+
+@export var snowfall_fps_limit: int = 20
+
+var last_shader_update: float
+var shader_time: float
+
+@onready var color_rect: ColorRect = $ColorRect
+@onready var snowfall_shader: ShaderMaterial = color_rect.material
+
+
+func _ready() -> void:
+	shader_time = snowfall_shader.get_shader_parameter("time")
+	last_shader_update = shader_time
+
+
+func _physics_process(delta: float) -> void:
+	shader_time += delta
+	if shader_time - last_shader_update > 1.0 / snowfall_fps_limit:
+		snowfall_shader.set_shader_parameter("time", shader_time)
+		last_shader_update = shader_time
