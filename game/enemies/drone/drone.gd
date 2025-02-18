2e671eb36b10470d8713c1cc5fa697d07c0af443
hatmix <67128187+hatmix@users.noreply.github.com>
2025-02-18 11:41:14 -0500

    Added drone search state and fixed player collision with trees

--- game/enemies/drone/drone.gd
+++ game/enemies/drone/drone.gd
@@ -10,18 +10,36 @@ extends Enemy  # Maybe not useful now...
 ##   trail ended, so enter search pattern from last point
 ##   (see https://owaysonline.com/iamsar-search-patterns/)
 
-@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
-@onready var _state: StateMachine = $StateMachine
+@export var search_speed: float = 15.0
+
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var detect_area_2d: Area2D = $DetectArea2D
+@onready var scanner_back: Sprite2D = $ScannerBack
+@onready var scanner_front: Sprite2D = $ScannerFront
+@onready var shadow: Sprite2D = $Shadow
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+@onready var _state: StateMachine = $StateMachine
+@onready var _patrol: Node = $StateMachine/Patrol
+@onready var _track: Node = $StateMachine/Track
+@onready var _search: Node = $StateMachine/Search
+
+
+func scan(enable: bool = true) -> void:
+	scanner_back.visible = enable
+	scanner_front.visible = enable
+	shadow.visible = !enable
 
 
 func _ready() -> void:
 	visible_on_screen_notifier_2d.screen_exited.connect(
 		_on_visible_on_screen_notifier_2d_screen_exited
 	)
+	detect_area_2d.body_entered.connect(_on_body_entered_detect_area)
+	detect_area_2d.area_entered.connect(_on_area_entered_detect_area)
 	_state.state_changed.connect(_update_state_label)
 	%StateLabel.text = _state.current_state.name
-
+	scan(false)
 	# set initial direction toward center of screen
 	direction = global_position.direction_to(Vector2(160, 90))
 	anim_player.play("default")
@@ -41,12 +59,24 @@ func _update_state_label(new_state: FsmState, _old_state: FsmState) -> void:
 	%StateLabel.text = new_state.name
 
 
-# TODO: try to figure out a good way to avoid this entanglement
+func _on_body_entered_detect_area(body: Node2D) -> void:
+	print(name, " detected ", body.name)
+	if body.name == "Player":
+		# For now, just change to search state until more tracking is worked out
+		_search.point_of_interest = body.global_position
+		_state.change_to(_search)
+
+
+func _on_area_entered_detect_area(area: Area2D) -> void:
+	print(name, " detected ", area.name, " belonging to ", area.get_parent().name)
+
+
+# TODO: figure out a good way to avoid this entanglement
 # notifier2D loses global_position if under FsmState nodes
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	match _state.current_state.name:
-		"Patrol":
+	match _state.current_state:
+		_patrol:
 			direction = global_position.direction_to(Vector2(160, 90))
-		"Track":
+		_track:
 			# Following something off the map...
 			queue_free()
--- game/enemies/drone/states/search.gd
+++ game/enemies/drone/states/search.gd
@@ -1,18 +1,46 @@
 extends FsmState
 
+var point_of_interest: Vector2
+var path: Array[Vector2]
+
+@onready var patrol_pattern: Line2D = $PatrolPattern
+
 
 func enter_state():
-	pass
+	# Rotate the search pattern by drone's travel direction
+	# consider whether it's better for player if it's never rotated...
+	var theta: float = Vector2.RIGHT.angle_to(_state.target.direction)
+	var points: PackedVector2Array = patrol_pattern.points
+	path = []
+	for point: Vector2 in points:
+		var rotated_point: Vector2
+		rotated_point.x = (point.x * cos(theta) - point.y * sin(theta) + point_of_interest.x)
+		rotated_point.y = (point.x * sin(theta) + point.y * cos(theta) + point_of_interest.y)
+		path.append(rotated_point)
+	_state.target.scan(true)
 
 
 func exit_state():
-	pass
+	_state.target.scan(false)
+	point_of_interest = Vector2.ZERO
+	path = []
 
 
 func process(_delta: float) -> void:
 	pass
 
 
-# TODO: handle obstacle avoidance
 func physics_process(_delta: float) -> void:
-	pass
+	if _state.target.global_position.distance_to(path[0]) < 10:
+		path.pop_front()
+
+	# If we've completed the search path, go back to patrolling
+	if path.size() == 0:
+		_state.change_to("Patrol")
+		return
+
+	_state.target.direction = _state.target.global_position.direction_to(path[0])
+	_state.target.velocity = _state.target.direction * _state.target.search_speed
+	# FIXME: handle obstacle avoidance--what if path[0] is inside a collider?
+	if _state.target.move_and_slide():
+		print(_state.target.name, " search pattern collision :(")
