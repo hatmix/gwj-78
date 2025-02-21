3bfdd66b91f5f97c1963d51f32e9d190e59e1742
hatmix <67128187+hatmix@users.noreply.github.com>
2025-02-21 14:33:27 -0500

    Refactored game.gd for start_level func

--- game/game.gd
+++ game/game.gd
@@ -3,18 +3,19 @@ extends Node
 const IN: String = "GradientVertical"
 const OUT: String = "GradientVerticalInverted"
 
+@export var first_level: PackedScene
 @export var default_mapping_context: GUIDEMappingContext
 @export var transition_pattern: Image
 
 var snow_color: Color = ProjectSettings.get_setting(
 	"rendering/environment/defaults/default_clear_color"
 )
+var map: Map
 
 @onready var bgm: AudioStreamPlayer = $Bgm
 @onready var game_over: Label = $UI/GameOver
 @onready var victory: Label = $UI/Victory
 @onready var map_placeholder: Node2D = $MapPlaceholder
-@onready var map: Map = $MapPlaceholder/Map_1
 
 
 func _ready() -> void:
@@ -25,6 +26,8 @@ func _ready() -> void:
 	victory.visible = false
 	$UI.show_ui("Game")
 	bgm.play()
+	await get_tree().process_frame
+	start_level(first_level)
 
 
 func _input(_event: InputEvent) -> void:
@@ -32,37 +35,42 @@ func _input(_event: InputEvent) -> void:
 		Global.player_won.emit()
 
 
+func fade_out(duration: float = 1.0) -> Fade:
+	return Fade.fade_out(duration, snow_color, OUT)
+
+
+func fade_in(duration: float = 1.0) -> Fade:
+	return Fade.fade_in(duration, snow_color, IN)
+
+
+func start_level(_map: PackedScene) -> void:
+	fade_out()
+	game_over.visible = false
+	victory.visible = false
+	map = _map.instantiate()
+	for child in map_placeholder.get_children():
+		child.queue_free()
+	map_placeholder.add_child.call_deferred(map)
+	GUIDE.disable_mapping_context(default_mapping_context)
+	GUIDE.enable_mapping_context(default_mapping_context)
+	fade_in()
+	get_tree().set_deferred("paused", false)
+
+
 func _lose() -> void:
 	game_over.visible = true
 	get_tree().paused = true
+	var redo: PackedScene = load(map.scene_file_path)
 	await get_tree().create_timer(2.0).timeout
-	await Fade.fade_out(1.0, snow_color, OUT).finished
-	Fade.fade_in(1.0, snow_color, OUT, true)
-	get_tree().set_deferred("paused", false)
-	# FIXME: re-instantiate the map, not the whole scene, and make the
-	# transition start at the player's position
-	get_tree().reload_current_scene()
+	start_level(redo)
 
 
-# TODO: use Fade or make some kind of nice level transition
 func _win() -> void:
 	victory.visible = true
 	get_tree().paused = true
 	await get_tree().create_timer(2.0).timeout
-	await Fade.fade_out(1.0, snow_color, OUT, true).finished
 	if map.next_level:
-		var next_level: Map = map.next_level.instantiate()
-		map.free()
-		map_placeholder.add_child(next_level)
-		map = next_level
-		victory.visible = false
-		Fade.fade_in(1.0, snow_color, IN)
-		# Will this prevent starting map with input triggered?
-		GUIDE.disable_mapping_context(default_mapping_context)
-		GUIDE.enable_mapping_context(default_mapping_context)
-
-		get_tree().set_deferred("paused", false)
+		start_level(map.next_level)
 	else:
-		Fade.fade_in(1.0, snow_color, IN)
-		get_tree().set_deferred("paused", false)
-		get_tree().reload_current_scene()
+		var redo: PackedScene = load(map.scene_file_path)
+		start_level(redo)
