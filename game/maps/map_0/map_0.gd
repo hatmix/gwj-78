95859eb02cf8c1435a3709c7b8c753dd76760197
hatmix <67128187+hatmix@users.noreply.github.com>
2025-02-23 14:40:14 -0500

    Updated tutorial, set start back to tutorial map

--- game/maps/map_0/map_0.gd
+++ game/maps/map_0/map_0.gd
@@ -1,1 +1,16 @@
 extends Map
+
+
+func _ready() -> void:
+	# pause weather for effect
+	if is_instance_valid(Global.weather):
+		Global.weather.snow_fade_duration = 0.0
+		Global.weather.stop_snowing()
+		Global.weather.weather_change_timer.process_mode = Node.PROCESS_MODE_DISABLED
+		Global.weather.set_deferred("snow_fade_duration", 3.0)
+	
+	
+func resume_weather() -> void:
+	# Needed to re-enable random weather that was disabled for effect above
+	Global.weather.weather_change_timer.process_mode = Node.PROCESS_MODE_INHERIT
+	Global.weather.weather_change_timer.start()
